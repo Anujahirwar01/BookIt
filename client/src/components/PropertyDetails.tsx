c575ac1365331e3b62cc943ee74a8ac21366e027
Anujahirwar01 <anujahirwar77010@gmail.com>
2025-10-31 12:59:19 +0530

    done

--- client/src/components/PropertyDetails.tsx
+++ client/src/components/PropertyDetails.tsx
@@ -450,7 +450,7 @@ const PropertyDetails: React.FC = () => {
                         {/* Left: Image + details */}
                         <div>
                             <div className='flex gap-50'>
-                                <div className="mb-4 w-90">
+                                <div className="mb-4 ">
                                     <img
                                         src={property?.image || 'https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80'}
                                         alt={property?.title || 'Property'}
